4b708a374bc0a19e332417156ec56e6ecd255868
Catboi <90759767+Mardanis@users.noreply.github.com>
2022-09-11 03:38:14 +0700

    Add files via upload

---
+++ Pengulan Data Nilai UAS & UTS Sederhana.py
@@ -0,0 +1,9 @@
+ulang = 2
+for i in range(ulang):
+    print ("data ke -" + str(i+1))    
+    
+    nama=input("Masukkan NIM anda : ")
+    uts=int(input("Masukkan Nilai UTS anda : "))
+    uas=int(input("Masukkan Nilai UAS anda : "))
+    print("NIM anda adalah %s nilai UTS anda %i nilai UTS anda %i" % (nama,uts,uas))
+    print("---------------------------------------\n")      
